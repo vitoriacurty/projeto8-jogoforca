4f5395fbff2e1bffdaeb4633f768cbc3de2a0daa
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-17 23:21:39 -0300

    add component App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { Jogo } from "./components/Jogo";
+import { Letras } from "./components/Letras";
+
 export default App;
 function App() {
   return (
-    <>App</>
+    <>
+    <Jogo />
+    <Letras />
+    </>
   );
 }
