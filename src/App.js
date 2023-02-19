398ac8685755caaa45458b27a28f22ea9cb599e0
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-19 18:32:48 -0300

    import array palavras

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { Jogo } from "./components/Jogo";
-import { Letras } from "./components/Letras";
+import { Jogo } from "./components/jogo/Jogo";
+import { Letras } from "./components/letras/Letras";
+import palavras from "./palavras";
 
 export default App;
 function App() {
+  const imagens = [
+    { src: "./assets/forca0.png" },
+    { src: "./assets/forca1.png" },
+    { src: "./assets/forca2.png" },
+    { src: "./assets/forca3.png" },
+    { src: "./assets/forca4.png" },
+    { src: "./assets/forca5.png" },
+    { src: "./assets/forca6.png" }
+  ]
+
   return (
     <>
-    <Jogo />
-    <Letras />
+      {imagens.map(item =>
+        < Jogo src={item.src} />
+      )
+      }
+
+      <Letras />
     </>
   );
 }
