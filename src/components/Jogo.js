b41d47ec040933eedbb485350186c6b7fc90cfdf
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-20 19:44:09 -0300

    add props and words style

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,16 +1,30 @@
 
-export const Jogo = (props) => {
-    return (
-        <div className="game-container">
-            <div className="game-left">
-                <img src={props.src} alt="imagem forca" />
-            </div>
+export const Jogo = ({ wrong, show, onClick, disabled, gameOver, win }) => {
+  const image = [
+    "../assets/forca0.png",
+    "../assets/forca1.png",
+    "../assets/forca2.png",
+    "../assets/forca3.png",
+    "../assets/forca4.png",
+    "../assets/forca5.png",
+    "../assets/forca6.png",
+  ]
 
-            <div className="game-right">
-                <button><p className="p-game">Escolher Palavra</p></button>
-                <h1>quarentena</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="game-container">
+      <div className="game-left">
+        <img src={image[wrong]} alt="imagem forca" />
+      </div>
+
+      <div className="game-right">
+        <button onClick={onClick}>
+          <p className="p-game">Escolher Palavra</p>
+        </button>
+        <h1 style={gameOver ? win ? { color: "green" } : { color: "red" } : { color: "black" }}>
+          {(!disabled || gameOver) ? (show ? show.join(" ") : "") : ""}
+        </h1>
+      </div>
+    </div>
+  )
 
 }
