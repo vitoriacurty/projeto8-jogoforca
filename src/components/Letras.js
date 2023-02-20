94d3c46f73a27f6d5f18a4fe568a9a10714f6a87
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-19 21:36:16 -0300

    add map in component Letras

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,6 +1,15 @@
 export const Letras = () => {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return (
-        <div className="letras"></div>
+        <div className="letras-container">
+            <div className="letra">
+                {alfabeto.map(item => {
+                    return <button key={item} disabled={true}>
+                        {item.toUpperCase()}
+                    </button>
+                }
+                )}
+            </div>
+        </div>
     )
 }
