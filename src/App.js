78a7f0a2f81d85663acb902315b69d85b05616a2
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-20 19:46:16 -0300

    import useState in components

--- src/App.js
+++ src/App.js
@@ -3,28 +3,61 @@ import { Jogo } from "./components/Jogo";
 import { Letras } from "./components/Letras";
 import palavras from "./palavras";
 
-const [disabled, setDisabled] = useState(true)
+export default function App() {
+  const [disabled, setDisabled] = useState(true)
+  const [wrong, setWrong] = useState(0)
+  const [word, setWord] = useState([])
+  const [show, setShow] = useState([])
+  const [pickedLetters, setPickedLetters] = useState([])
+  const [gameOver, setGameOver] = useState(false)
+  const [win, setWin] = useState(false)
 
-export default App;
-function App() {
-  const imagens = [
-    /*-- { src: "./assets/forca0.png" },
-    { src: "./assets/forca1.png" },
-    { src: "./assets/forca2.png" },
-    { src: "./assets/forca3.png" },
-    { src: "./assets/forca4.png" },
-    { src: "./assets/forca5.png" }, --*/
-    { src: "./assets/forca6.png" }
-  ]
+  function button() {
+    setWrong(0);
+    setDisabled(false);
+    setGameOver(false)
+    setWin(false)
+    const newWord = palavras[Math.floor(Math.random() * (palavras.length - 1))]
+    setWord(newWord.split(""))
+    setShow(Array(newWord.length).fill('_'))
+    setPickedLetters([])
+  }
+
+  function handleButton(item) {
+    setPickedLetters([...pickedLetters, item])
+
+    const normalize = word.join("").normalize('NFD').replace(/[\u0300-\u036f]/g, "").split("")
+    if (normalize.includes(item.toLowerCase())) {
+      let newShow = [...show];
+      normalize.forEach((letter, pos) => {
+        if (item === letter)
+          newShow[pos] = word[pos]
+      })
+      setShow(newShow)
+    }
+    else {
+      const newWrong = wrong + 1
+      setWrong(newWrong)
+    }
+  }
+
+  function endGame(endGame) {
+    setShow(word.join("").split(""))
+    setGameOver(true)
+    setDisabled(true)
+    endGame ? setWin(true) : setWin(false)
+  }
+
+  if (wrong === 6 && !disabled) {
+    endGame(false)
+  } else if (show.join("") === word.join("") && !disabled) {
+    endGame(true)
+  }
 
   return (
     <main>
-      {imagens.map(item =>
-        < Jogo src={item.src} />
-      )
-      }
-
-      <Letras disabled={disabled} />
+      <Jogo wrong={wrong} show={show} onClick={() => button()} disabled={disabled} gameOver={gameOver} win={win} />
+      <Letras disabled={disabled} pickedLetters={pickedLetters} handleButton={handleButton} />
     </main>
   );
 }
