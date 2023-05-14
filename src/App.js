fa8aeed405b14b6a8135dc56aa4f02aea1bdd053
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-13 23:07:14 -0300

    implementação do requisito de mostrar letras

--- src/App.js
+++ src/App.js
@@ -7,20 +7,40 @@ export default function App() {
   const [wrong, setWrong] = useState(0)
   const [word, setWord] = useState([])
   const [show, setShow] = useState([])
+  const [pickedLetters, setPickedLetters] = useState([])
+  const [disabled, setDisabled] = useState(true)
 
   function button() {
     setWrong(0)
+    setDisabled(false)
     const newWord = palavras[Math.floor(Math.random() * (palavras.length - 1))]
     setWord(newWord.split(""))
     setShow(Array(newWord.length).fill(' _'))
+    setPickedLetters([])
     console.log(newWord)
-
   }
 
-  return (
-    <main>
-      <Jogo wrong={wrong} show={show} onClick={() => button()} />
-      <Letras />
-    </main>
-  )
+  function handleButton(item) {
+    setPickedLetters([...pickedLetters, item])
+
+    const normalize = word.join("").normalize('NFD').replace(/[\u0300-\u036f]/g, "").split("")
+    if (normalize.includes(item.toLowerCase())) {
+      let newShow = [...show];
+      normalize.forEach((letter, pos) => {
+        if (item === letter)
+          newShow[pos] = word[pos]
+      })
+      setShow(newShow)
+    } else {
+      const newWrong = wrong + 1
+      setWrong(newWrong)
+    }
+  }
+
+return (
+  <main>
+    <Jogo wrong={wrong} show={show} onClick={() => button()} disabled={disabled} />
+    <Letras pickedLetters={pickedLetters} handleButton={handleButton} disabled={disabled} />
+  </main>
+)
 }
--- src/componentes/Letras.js
+++ src/componentes/Letras.js
@@ -1,10 +1,15 @@
-export const Letras = ({}) => {
+export const Letras = ({ disabled, pickedLetters, handleButton }) => {
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   return (
     <div className="letras-container">
       <div className="letra">
-        {alfabeto.map((item) => <button key={item} disabled={true}>{item}</button>)}
+        {alfabeto.map(item => {
+          const isPicked = pickedLetters.includes(item)
+          return <button key={item} onClick={() => handleButton(item)} disabled={disabled || isPicked}>
+            {item.toUpperCase()} 
+          </button>
+        })}
       </div>
     </div>
   )
